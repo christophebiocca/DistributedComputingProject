803121526c0c83a96890add86c8857ed18a45624
Drew Gross <drew.a.gross@gmail.com>
2011-09-17 00:25:38 -0400

    webserver with range, client responds

--- Client/Client.py
+++ Client/Client.py
@@ -2,14 +2,19 @@ import urllib.request
 import subprocess
 import sys
 import urllib
+import string
 
-start = int(sys.stdin.readline())
-end = int(sys.stdin.readline())
 
-usercode = subprocess.Popen("TestProgram.exe " + str(start) + " " + str(end), stdout=subprocess.PIPE)
 while True:
-    line = usercode.stdout.readline() 
-    if line:
-        print(int(line))
-    else:
-        break
+    range = list(map(int, urllib.request.urlopen("http://localhost:1080").read().split()))
+
+    start = range[0]
+    end = range[1]
+
+    usercode = subprocess.Popen("TestProgram.exe " + str(start) + " " + str(end), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    while True:
+        line = usercode.stdout.readline() 
+        if line:
+            print(int(line))
+        else:
+            break
--- Server/Server.py
+++ Server/Server.py
@@ -1,13 +1,42 @@
-import urllib.request
 import subprocess
 import sys
-import urllib
+import http
+import http.server
+import cgi
 
 
+class MyRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write("10\n20".encode("utf-8"))
+        return
 
-clientInterface = subprocess.Popen(r"C:\Python32\python.exe ..\Client\Client.py", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            query = cgi.parse_multipart(self.rfile, pdict)
+        self.send_response(301)
+        self.end_headers()
 
-sys.stdout.writelines("lol")
+        upfilecontent = query.get('upfile')
+        print("content: ", upfilecontent[0])
+        self.wfile.write("<HTML>POST OK</HTML>".encode("utf-8"))
+        self.wfile.write(upfilecontent[0].encode("utf-8"))
+
+server = http.server.HTTPServer(('', 1080), MyRequestHandler)
+print("Started server")
+server.serve_forever()
+print("end")
+
+
+clientInterface = subprocess.Popen(r"dist\Client.exe", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+print(clientInterface.stdin)
+
+clientInterface.stdin.write("10\n".encode("utf-8"))
+clientInterface.stdin.write("20\n".encode("utf-8"))
 
 while True:
     line = clientInterface.stdout.readline()
