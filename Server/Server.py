86e6ca3fe99223416a8377d4fa03c003a031ee02
Drew Gross <drew.a.gross@gmail.com>
2011-09-17 03:42:11 -0400

    gets autoincrementing ranges from server

--- Server/Server.py
+++ Server/Server.py
@@ -4,28 +4,34 @@ import http
 import http.server
 import cgi
 
+def MyClosure():
+    processed = [0]
 
-class MyRequestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("10\n20".encode("utf-8"))
-        return
+    class MyRequestHandler(http.server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            range = (str(processed[0]) + "\n" + str(processed[0] + 10)).encode("utf-8")
+            processed[0] += 10
+            self.wfile.write(range)
+            return
 
-    def do_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-        if ctype == 'multipart/form-data':
-            query = cgi.parse_multipart(self.rfile, pdict)
-        self.send_response(301)
-        self.end_headers()
+        def do_POST(self):
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                query = cgi.parse_multipart(self.rfile, pdict)
+            self.send_response(301)
+            self.end_headers()
 
-        upfilecontent = query.get('upfile')
-        print("content: ", upfilecontent[0])
-        self.wfile.write("<HTML>POST OK</HTML>".encode("utf-8"))
-        self.wfile.write(upfilecontent[0].encode("utf-8"))
+            upfilecontent = query.get('upfile')
+            print("content: ", upfilecontent[0])
+            self.wfile.write("<HTML>POST OK</HTML>".encode("utf-8"))
+            self.wfile.write(upfilecontent[0].encode("utf-8"))
 
-server = http.server.HTTPServer(('', 1080), MyRequestHandler)
+    return MyRequestHandler
+
+server = http.server.HTTPServer(('', 1080), MyClosure())
 print("Started server")
 server.serve_forever()
 print("end")
