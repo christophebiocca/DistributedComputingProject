0efbf70861bb5beb669c3f8c3d033cad2ca020d0
U-Jake-Acer-Aspir\Jake <Jake@Jake-Acer-Aspir.(none)>
2011-09-18 12:19:39 -0400

    Pulled from master

---
+++ DistributedProjects/TestProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = Convert.ToInt32(args[0]);
+            int end = Convert.ToInt32(args[1]);
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Server/PostRequestMethods.py
@@ -0,0 +1,40 @@
+"""Methods on the server that can be called from
+the client by sending a POST request with a "request"
+paramater that has a value of the name of the method"""
+import pickle
+import urllib
+
+import ServerState
+
+state = ServerState.ServerState()
+currentProject = "CrossPlatformTestProgram"
+
+def newProgram(requestHandler, clientData):
+    clientPlatform = pickle.loads(urllib.parse.unquote_to_bytes(clientData.getvalue("platform")))
+    requestHandler.send_response(200)
+    requestHandler.send_header("Content-Type", "application/octet-stream")
+    requestHandler.end_headers()
+    
+    if currentPlatform == "posix":
+        requestHandler.wfile.write(open("../DistributedProjects/" + currentProject + "/bin/" + currentProject + ".linux", "rb").read())
+    if currentPlatform == "nt":
+        requestHandler.wfile.write(open("../DistributedProjects/" + currentProject + "/bin/" + currentProject + ".exe", "rb").read())
+    if currentPlatform == "mac":
+        requestHandler.wfile.write(open("../DistributedProjects/" + currentProject + "/bin/" + currentProject + ".mac", "rb").read())
+
+def newRange(requestHandler, clientData):
+    requestHandler.send_response(200)
+    requestHandler.send_header('Content-Type', 'text/plain')
+    requestHandler.end_headers()
+    range = state.nextData()
+    data = (str(range[0]) + "\n" + str(range[1]) + "\n").encode("utf-8")
+    requestHandler.wfile.write(data)
+
+def reportValues(requestHandler, clientData):
+    range = clientData.getvalue("computedRange")
+    matchingValues = pickle.loads(urllib.parse.unquote_to_bytes(clientData.getvalue("matchingValues")))
+
+    state.aggregateResults(matchingValues)
+
+    requestHandler.send_response(200)
+    requestHandler.end_headers()
