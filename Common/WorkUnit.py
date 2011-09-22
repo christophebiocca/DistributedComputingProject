df8a9991df0e6ae6b47204a92daf6c6b44cc2a17
Drew Gross <drew.a.gross@gmail.com>
2011-09-21 21:49:48 -0400

    fixed up folder structure

--- Common/WorkUnit.py
+++
@@ -1,7 +0,0 @@
-class WorkUnit:
-    """The data to sent to a node to process"""
-    def init(self, start, end):
-        self.workUnitProtocolVersion = 1
-
-        self.rangeStart = start
-        self.rangeEnd = end
